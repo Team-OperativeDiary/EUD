9f72677d680d3839b9e21825b696ec22ed06e085
NarhiAkseli <narhiaks@gmail.com>
2024-03-18 13:43:06 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:aDbGfCCd533Da-CHcfgdGf3-Cg1hH1FF@roundhouse.proxy.rlwy.net:49193/railway'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
